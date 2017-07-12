58a5e749200cea15c33b59b80b4d7f66868e8d05
jakiir <jakir44.du@gmail.com>
2017-07-12 23:15:19 +0600

    Edit template

--- themes/realEstate/page-template.php
+++ themes/realEstate/page-template.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
- *
+ * Template Name: Add Template Page 
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
