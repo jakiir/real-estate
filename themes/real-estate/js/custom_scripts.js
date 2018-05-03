cbb54ea62fa7b42719b2c509ac20c03a0e1b7b22
jakiir <jakir44.du@gmail.com>
2018-05-03 13:40:41 +0600

    remove template

--- themes/real-estate/js/custom_scripts.js
+++ themes/real-estate/js/custom_scripts.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function(){
-	jQuery('#form-data').DataTable();
+	jQuery('#template-data').DataTable();
+	
+    jQuery(".trigger_popup_fricc").click(function(){
+		var template_id = jQuery(this).attr('data-template');
+		jQuery("#get_template_id").val(template_id);
+       jQuery('.hover_bkgr_fricc').show();
+    });
+    /*jQuery('.hover_bkgr_fricc').click(function(){
+        jQuery('.hover_bkgr_fricc').hide();
+    });*/
+    jQuery('.popupCloseButton').click(function(){
+        jQuery('.hover_bkgr_fricc').hide();
+    });	
 });
